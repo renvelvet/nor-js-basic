529cbfbb040a8bbd79878d43bedd77508feea053
Resha Puspita <reshapuspita734@gmail.com>
2020-06-11 11:06:29 +0700

    update data diri

--- 1-data-diri.js
+++ 1-data-diri.js
@@ -1,5 +1,11 @@
-console.log("Nama: Resha Puspita Dewi")
-console.log("Asal Daerah: Bandung")
-console.log("Tanggal lahir: 7 Mei 1998")
-console.log("Umur: 22 tahun")
-console.log("Posisi pekerjaan: Backend Developer")
+let name = "Resha Puspita Dewi"
+let city = "Bandung"
+let birthdate = "7 Mei 1998"
+let age = 22
+let job = "Backend Developer"
+
+console.log("Name: " + name)
+console.log("Asal Daerah: " + city)
+console.log("Tanggal lahir: " + birthdate)
+console.log("Umur : " + age + " tahun")
+console.log("Posisi pekerjaan:" + job)
